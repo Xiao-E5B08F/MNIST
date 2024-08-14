65143e959e4b7c3ff3302035e2b8ceac2235808c
Xiao <Jack98086gan@gmail.com>
2024-08-14 15:00:53 +0800

    feat: Update dataset in main.py

--- main.py
+++ main.py
@@ -162,8 +162,8 @@ def main():
     ])
 
     # Load dataset
-    train_dataset = datasets.FashionMNIST('./data', train=True, download=True, transform=transform)
-    test_dataset = datasets.FashionMNIST('./data', train=False, transform=transform)
+    train_dataset = datasets.MNIST('./data', train=True, download=True, transform=transform)
+    test_dataset = datasets.MNIST('./data', train=False, transform=transform)
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
